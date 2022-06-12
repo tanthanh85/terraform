c7475452544bcfaa45bb657734e1d98212427dc7
Andy Thanh <tanthanh2285@gmail.com>
2022-06-12 04:51:12 +0000

    final config for Terraform aci lab

--- main.tf
+++ main.tf
@@ -14,36 +14,97 @@ provider "aci" {
   # APIC Password
   # password = var.user.password
   # APIC URL
-  cert_name = "andy_cert"
+  cert_name   = "andy_cert"
   private_key = "admin.key"
-  url      = var.user.url
-  insecure = true
+  url         = var.user.url
+  insecure    = true
 }
 
 # Define an ACI Tenant Resource.
 resource "aci_tenant" "devnet_tenant" {
-    name        = var.tenant
-    description = "This is devnet tennant"
+  name        = var.tenant
+  description = "This is devnet tennant"
 }
 
 resource "aci_vrf" "devnet_vrf" {
-    name = var.vrf
-    tenant_dn = aci_tenant.devnet_tenant.id
-    description = "This is VRF under devnet tenant"
+  name        = var.vrf
+  tenant_dn   = aci_tenant.devnet_tenant.id
+  description = "This is VRF under devnet tenant"
 }
 
 resource "aci_bridge_domain" "devnet_bd" {
-  for_each = var.bd
-  name = each.value.name
-  ip_learning = each.value.ip_learning
-  tenant_dn = aci_tenant.devnet_tenant.id 
+  for_each           = var.bd
+  name               = each.value.name
+  ip_learning        = each.value.ip_learning
+  tenant_dn          = aci_tenant.devnet_tenant.id
   relation_fv_rs_ctx = aci_vrf.devnet_vrf.id
 }
 
 
+
+
 resource "aci_subnet" "devnet_subnet" {
-  for_each = var.bd
+  for_each  = var.bd
   parent_dn = aci_bridge_domain.devnet_bd[each.key].id
-  ip = each.value.subnet
-  scope = each.value.subnet_scope
+  ip        = each.value.subnet
+  scope     = each.value.subnet_scope
+}
+
+
+resource "aci_filter" "devnet_filter" {
+  for_each  = var.filters
+  tenant_dn = aci_tenant.devnet_tenant.id
+  name      = each.value.filter
+
+}
+
+resource "aci_filter_entry" "devnet_filter_entry" {
+  for_each    = var.filters
+  filter_dn   = aci_filter.devnet_filter[each.key].id
+  name        = each.value.entry
+  ether_t     = "ipv4"
+  prot        = each.value.protocol
+  d_from_port = each.value.port
+  d_to_port   = each.value.port
+
+}
+
+resource "aci_contract" "devnet_contract" {
+  for_each    = var.contracts
+  tenant_dn   = aci_tenant.devnet_tenant.id
+  name        = each.value.contract
+  description = "Contract created by Terraform"
+}
+
+
+resource "aci_contract_subject" "devnet_contract_subject" {
+  for_each                     = var.contracts
+  contract_dn                  = aci_contract.devnet_contract[each.key].id
+  name                         = each.value.subject
+  relation_vz_rs_subj_filt_att = [aci_filter.devnet_filter[each.value.filter].id]
+
+}
+
+
+resource "aci_l2_outside" "devnet_l2out" {
+  tenant_dn   = aci_tenant.devnet_tenant.id
+  name        = "devnet_layer2_out"
+  description = "l2out created by Terraform"
+}
+resource "aci_tenant" "tenant_for_rest_example" {
+  name        = "tenant_for_rest"
+  description = "This tenant is created by terraform ACI provider"
+}
+
+resource "aci_rest" "rest_l3_ext_out" {
+  path       = "/api/node/mo/${aci_tenant.tenant_for_rest_example.id}/out-test_ext.json"
+  class_name = "l3extOut"
+  content = {
+    "name" = "test_ext"
+  }
+}
+
+data "aci_vrf" "data_devnet_vrf" {
+  tenant_dn = aci_tenant.devnet_tenant.id
+  name      = "devnet_vrf"
 }
--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "user" {
   description = "Login information"
-  type        = map
-  default     = {
+  type        = map(any)
+  default = {
     username = "admin"
     password = "!v3G@!4@Y"
     url      = "https://sandboxapicdc.cisco.com"
@@ -11,37 +11,37 @@ variable "user" {
 
 variable "tenant" {
   description = "tennant information"
-  type = string
-  default = "devnet_tenant"
+  type        = string
+  default     = "devnet_tenant"
 }
 
 
 variable "vrf" {
-  type = string
+  type    = string
   default = "devnet_vrf"
 }
 
 variable "bd" {
-  type = map
+  type = map(any)
   default = {
     web_bd = {
-      name = "web_bd"
-      ip_learning = "yes"
-      subnet = "10.1.1.1/24"
+      name         = "web_bd"
+      ip_learning  = "yes"
+      subnet       = "10.1.1.1/24"
       subnet_scope = ["private"]
-      
+
     },
     app_bd = {
-      name = "app_bd"
-      ip_learning = "yes"
-      subnet = "10.2.2.1/24"
+      name         = "app_bd"
+      ip_learning  = "yes"
+      subnet       = "10.2.2.1/24"
       subnet_scope = ["private"]
 
     },
     db_bd = {
-      name = "db_bd"
-      ip_learning = "yes"
-      subnet = "10.3.3.1/24"
+      name         = "db_bd"
+      ip_learning  = "yes"
+      subnet       = "10.3.3.1/24"
       subnet_scope = ["private"]
     }
   }
@@ -50,21 +50,49 @@ variable "bd" {
 
 
 variable "filters" {
-  type = map
+  type = map(any)
   default = {
     filter_https = {
-      filter = "https",
-      entry = "https",
+      filter   = "https",
+      entry    = "https",
       protocol = "tcp",
-      port = "443"
+      port     = "443"
 
     },
     filter_sql = {
-      filter = "sql",
-      entry = "sql",
+      filter   = "sql",
+      entry    = "sql",
       protocol = "tcp",
-      port = "1433"
+      port     = "1433"
     }
 
+  }
+}
+
+
+variable "contracts" {
+  description = "This is all devnet contracts belong to devnet tenant"
+  type        = map(any)
+  default = {
+    contract_web = {
+      contract = "web",
+      subject  = "https",
+      filter   = "filter_https"
+    },
+    contact_sql = {
+      contract = "sql",
+      subject  = "sql",
+      filter   = "filter_sql"
+    }
+  }
+}
+variable "l3out" {
+  type = object({
+    name        = string
+    description = string
+  })
+  default = {
+    name        = "corp_l3"
+    description = "Created Using Terraform"
   }
 }
